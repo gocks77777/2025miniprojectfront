00e425b79ee92b129f9d2fa47b43de9686539a12
Minjae Kwon <marker1005@gmail.com>
2025-05-28 03:32:25 +0900

    branch fixing

--- src/mypage/EditInfo.jsx
+++ src/mypage/EditInfo.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from '../api/axiosInstance';
 import { useNavigate } from 'react-router-dom';
-import './EditInfo.css';
+//import './EditInfo.css';
 
 export default function EditInfo() {
   const [name, setName] = useState('');
