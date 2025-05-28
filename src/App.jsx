0164799ec9e4c075df482489fbea66d4dec50388
Minjae Kwon <marker1005@gmail.com>
2025-05-28 21:35:46 +0900

    App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,8 @@ import Login from './User/Login';
 import Signup from './User/Signup';
 import Dashboard from './Mypage/Dashboard';
 import EditInfo from './Mypage/EditInfo';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Study from './Study/test';
 import FileUpload from './AI/fileUpload';
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
-=======
->>>>>>> refs/remotes/origin/feature/AI
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
 
 export default function App() {
   console.log("앱 렌더링");
@@ -32,40 +17,9 @@ export default function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/mypage/edit" element={<EditInfo />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         <Route path="/study" element={<Study />} />
         <Route path="/fileupload" element={<FileUpload />} />
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
-=======
->>>>>>> refs/remotes/origin/feature/AI
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
       </Routes>
     </BrowserRouter>
   );
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      </Routes>
-    </BrowserRouter>
-  );
-}
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
-=======
->>>>>>> refs/remotes/origin/feature/AI
-=======
->>>>>>> 6e67a1c374769feb343e8fb9c044e50826654ec7
